9f446ce7ba0cc7eb82c0c6c5ac8364e3174f147e
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-01-26 14:53:42 +0330

    Before merge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useRef, useEffect } from "react";
 import './App.css';
-import Product from "./Components/Product/Product";
 
 
 function App() {
 
   return (
       <>
-          <Product />
-          <Product />
-          <Product />
       </>
   )
 
