67ad764fe8605c366cbef171fdac31751cb6e566
Shahram-hajizaki <shahramhjz75@gmail.com>
2023-01-26 14:46:21 +0330

    build-form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React,{useState,useRef,useEffect} from "react";
 import './App.css';
 import styled from "styled-components";
+import Form from "./Components/Form/Form";
 
 
 function App() {
 
   return (
       <>
-
+         <Form/>
       </>
   )
 }
