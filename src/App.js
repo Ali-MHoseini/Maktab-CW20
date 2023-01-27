91b15369943a9080603345ec5b5db2787db20bfc
Morteza Javanbakht <114153047+MortezaJavanbakht@users.noreply.github.com>
2023-01-27 14:12:36 +0330

    resolve auto rerendering

--- src/App.js
+++ src/App.js
@@ -1,65 +1,58 @@
-import React, {useState, useRef, useEffect, createContext} from "react";
-import './App.css';
+import React, { useState, useRef, useEffect, createContext } from "react";
+import "./App.css";
 import useFetchAll from "./Hooks/useFetchAll";
 import Product from "./Components/Product/Product";
 import Form from "./Components/Form/Form";
-import useAddNew from "./Hooks/useAddNew";
-
-
 
 function App() {
-    const allCards = createContext()
-    const [newData,setNewData] = useState({title:"",description:""})
-    const [isAdd,setIsAdd] = useState(false)
-    const [data] = useFetchAll("https://63a3f3dc9704d18da099a375.mockapi.io/test");
-    const [mainData,setMainData] = useState([])
+  const [newData, setNewData] = useState({ title: "", description: "" });
+  const [isAdd, setIsAdd] = useState(false);
+  const [mainData, setMainData] = useState([]);
+  useFetchAll("https://63a3f3dc9704d18da099a375.mockapi.io/test", setMainData);
 
-
-
-    useEffect(()=> {
-        if(isAdd){
-            fetch('https://63a3f3dc9704d18da099a375.mockapi.io/test', {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;charset=utf-8",
-                },
-                body: JSON.stringify(newData),
-            })
-                .then(()=>setIsAdd(false))
-                .then(()=>setMainData([]))
-        }
-    },[isAdd])
-
-    const addToTitle = (value)=> {
-        setNewData({...newData,title: value});
+  useEffect(() => {
+    let resopnseData;
+    if (isAdd) {
+      fetch("https://63a3f3dc9704d18da099a375.mockapi.io/test", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(newData),
+      })
+        .then((res) => res.json())
+        .then((data) => (resopnseData = data))
+        .then(() => setIsAdd(false))
+        .then(() => setMainData([...mainData, resopnseData]));
     }
-    const addToDescription = (value)=> {
-        setNewData({...newData,description: value})
-    }
+  }, [isAdd]);
+
+  const addToTitle = (value) => {
+    setNewData({ ...newData, title: value });
+  };
+  const addToDescription = (value) => {
+    setNewData({ ...newData, description: value });
+  };
 
   return (
-      <allCards.Provider value={{mainData}}>
-      <div className='App'>
-          <div className='grid'>
-              {
-                  mainData && mainData.map(item=> (
-                      <Product
-                          key={item.id}
-                          title={item.title}
-                          description={item.description}
-                      />
-                  ))
-              }
-          </div>
-          <Form
-              AddTitle={(e)=>addToTitle(e.target.value)}
-              AddDescription={(e)=>addToDescription(e.target.value)}
-              setData={()=>setIsAdd(true)}
-          />
+    <div className="App">
+      <div className="grid">
+        {mainData &&
+          mainData.map((item) => (
+            <Product
+              key={item.id}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
       </div>
-      </allCards.Provider>
-  )
-
+      <Form
+        AddTitle={(e) => addToTitle(e.target.value)}
+        AddDescription={(e) => addToDescription(e.target.value)}
+        setData={() => setIsAdd(true)}
+      />
+    </div>
+  );
 }
 
 export default App;
